ca736eeef34465c70e173f933864e994d17587b9
Thomas Stahl <me@tmmy.cc>
2024-11-23 20:14:40 +0100

    feat(kitty): enable gnome style window title

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -23,7 +23,8 @@
     };
     theme = "Tokyo Night";
     settings = {
-      hide_window_decorations = true;
+      hide_window_decorations = false;
+      linux_display_server = "x11";
       copy_on_select = true;
 
       mouse_hide_wait = "2.0";
