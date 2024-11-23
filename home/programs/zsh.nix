3f4589a733b38c2669c080458dcaedc441cc39bc
Thomas Stahl <me@tmmy.cc>
2024-11-23 20:13:59 +0100

    feat(zsh): enabled prefixed, unique history search, add some key bindings

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -13,10 +13,21 @@
     syntaxHighlighting.enable = true;
     initExtra = ''
       source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-      bindkey '^[OA' history-substring-search-up
-      bindkey '^[[A' history-substring-search-up
-      bindkey '^[OB' history-substring-search-down
-      bindkey '^[[B' history-substring-search-down
+      HISTORY_SUBSTRING_SEARCH_PREFIXED=1
+      HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+
+      bindkey '^[OA' history-substring-search-up    # Up
+      bindkey '^[[A' history-substring-search-up    # Up
+      bindkey '^[OB' history-substring-search-down  # Down
+      bindkey '^[[B' history-substring-search-down  # Down
+
+      bindkey '^[[1;5C' forward-word                # C-Right
+      bindkey '^[0c'    forward-word                # C-Right
+      bindkey '^[[5C'   forward-word                # C-Right
+
+      bindkey '^[[1;5D' backward-word               # C-Left
+      bindkey '^[0d'    backward-word               # C-Left
+      bindkey '^[[5D'   backward-word               # C-Left
     '';
   };
 }
