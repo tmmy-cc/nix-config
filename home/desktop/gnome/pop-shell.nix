7286a1930790db853c7a7a42b17ff5ac9505c846
Thomas Stahl <thomas.stahl@imar-navigation.de>
2024-11-25 08:19:22 +0100

    feat(pop-shell): add dconf settings

--- home/desktop/gnome/pop-shell.nix
+++ home/desktop/gnome/pop-shell.nix
@@ -4,4 +4,17 @@
   home.packages = with pkgs; [
     gnomeExtensions.pop-shell
   ];
+
+  dconf.settings = {
+    "org/gnome/shell/extensions/pop-shell" = {
+      active-hint = true;
+      fullscreen-launcher = true;
+      gap-inner = 2;
+      gap-outer = 2;
+      mouse-cursor-focus-location = 1;
+      show-skip-taskbar = false;
+      show-title = false;
+      tile-by-default = true;
+    };
+  };
 }
