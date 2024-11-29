6bed7d0c8118fc40e14aa2c05899ec6f55e7de8c
Thomas Stahl <me@tmmy.cc>
2024-11-29 13:21:20 +0100

    fix: remove unused allowUnfree from system specific configuration

--- nixos/tmmy-yoga/configuration.nix
+++ nixos/tmmy-yoga/configuration.nix
@@ -103,9 +103,6 @@
     enable = true;
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
