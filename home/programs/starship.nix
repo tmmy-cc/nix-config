1c51626e36b257e708b536fa9ac4a2902f6db721
Thomas Stahl <me@tmmy.cc>
2025-01-23 15:51:39 +0100

    fix(starship): fix zsh integration

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -4,18 +4,9 @@
   # Enable starship.rs
   programs.starship = {
     enable = true;
-    # Configuration written to ~/.config/starship.toml
+    enableZshIntegration = true;
     settings = {
       aws.disabled = true;
-
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
     };
   };
 }
