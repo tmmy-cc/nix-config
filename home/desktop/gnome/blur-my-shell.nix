540f9d0a6afddb7084a46d3e3b91bf398fd0e4e5
Thomas Stahl <me@tmmy.cc>
2025-06-08 19:47:32 +0200

    feat(blur-my-shell): add configuration

--- home/desktop/gnome/blur-my-shell.nix
+++ home/desktop/gnome/blur-my-shell.nix
@@ -4,4 +4,18 @@
   home.packages = with pkgs; [
     gnome.blur-my-shell
   ];
+
+  dconf.settings = {
+    "org/gnome/shell/extensions/blur-my-shell" = {
+      "blacklist"="@as []";
+      "blur-on-overview"=false;
+      "brightness"="1.0";
+      "customize"=true;
+      "enable-all"=true;
+      "opacity"="250";
+      "sigma"="59";
+      "blur"=true;
+    };
+  };
 }
+
